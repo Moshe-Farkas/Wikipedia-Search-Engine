3931e11ce5003a14c8fdc877b2ec5af37100d319
moshe <moishyfarkas9@gmail.com>
2024-05-30 11:56:10 +0300

    forgot to uncomment test stuff in main

--- main.go
+++ main.go
@@ -1,56 +1,46 @@
-// package main
-
-// import (
-// 	"flag"
-// 	"os"
-// 	"os/signal"
-// 	"project/src"
-// 	"strings"
-// )
-
-// const (
-// 	QUERY_MODE   = "query"
-// 	INDEX_MODE   = "index"
-// )
-
-// var (
-// 	mode         *string
-// )
-
-// func handleQuit() {
-// 	c := make(chan os.Signal, 1)
-// 	signal.Notify(c, os.Interrupt)
-// 	<- c
-// 	if *mode == INDEX_MODE {
-// 		src.StopIndexingSession()
-// 	} else if *mode == QUERY_MODE {
-// 		src.CloseDB()
-// 		os.Exit(0)
-// 	}
-// }
-
-// func main() {
-// 	go handleQuit()
-// 	mode = flag.String("mode", QUERY_MODE, "idk")
-// 	flag.Parse()
-// 	src.StartDB()
-// 	switch *mode {
-// 	case QUERY_MODE:
-// 		src.StartHandlingQueries()
-// 	case INDEX_MODE:
-// 		initialLink := os.Args[len(os.Args)-1]
-// 		initialLink = strings.ReplaceAll(initialLink, `'`, "")
-// 		src.StartIndexingSession(initialLink)
-// 		src.CloseDB()
-// 	}
-// }
-
-package main 
+package main
 
 import (
+	"flag"
+	"os"
+	"os/signal"
 	"project/src"
+	"strings"
 )
 
+const (
+	QUERY_MODE   = "query"
+	INDEX_MODE   = "index"
+)
+
+var (
+	mode         *string
+)
+
+func handleQuit() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<- c
+	if *mode == INDEX_MODE {
+		src.StopIndexingSession()
+	} else if *mode == QUERY_MODE {
+		src.CloseDB()
+		os.Exit(0)
+	}
+}
+
 func main() {
-	src.Test()
+	go handleQuit()
+	mode = flag.String("mode", QUERY_MODE, "idk")
+	flag.Parse()
+	src.StartDB()
+	switch *mode {
+	case QUERY_MODE:
+		src.StartHandlingQueries()
+	case INDEX_MODE:
+		initialLink := os.Args[len(os.Args)-1]
+		initialLink = strings.ReplaceAll(initialLink, `'`, "")
+		src.StartIndexingSession(initialLink)
+		src.CloseDB()
+	}
 }
